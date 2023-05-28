bb8b321a420e4da799785f5540caff2c70dbde39
Nicholas Berriochoa <nberriochoa@gmail.com>
2023-05-28 13:07:18 -0600

    Update automatic.py

--- automatic.py
+++ automatic.py
@@ -45,22 +45,25 @@ sys.stdout.write('Checking google credentials...')
 SCOPES = ['https://mail.google.com/']
 
 credentials = None
+home_folder = os.path.expanduser('~')
+pickle_path = os.path.join(home_folder, 'token.pickle')
+cred_path = os.path.join(home_folder, 'credentials.json')
 
 # check if credentials already exist
-if os.path.exists('~/token.pickle'):
-    with open('~/token.pickle', 'rb') as token:
+if os.path.exists(pickle_path):
+    with open(pickle_path, 'rb') as token:
         credentials = pickle.load(token)
 else:
     # load credentials from JSON file downloaded from Google Developers Console
-    if not os.path.exists('~/credentials.json'):
+    if not os.path.exists(cred_path):
         sys.stdout.write('missing credentials.json file')
         exit()
         
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file(cred_path, SCOPES)
     credentials = flow.run_local_server(port=0)
     
     # Save credentials for future use
-    with open('~/token.pickle', 'wb') as token:
+    with open(pickle_path, 'wb') as token:
         pickle.dump(credentials, token)
 
 sys.stdout.write('complete!\n')
